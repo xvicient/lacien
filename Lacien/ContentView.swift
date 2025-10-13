7ea36881b0fe439fe5402500e62ac0d0498bbdbe
Xavier Vicient <xavier.vicient@free-now.com>
2025-10-13 10:58:55 +0200

    Initial Commit

---
+++ Lacien/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lacien
+//
+//  Created by Xavier Vicient on 13/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lacien/LacienApp.swift
@@ -0,0 +1,17 @@
+//
+//  LacienApp.swift
+//  Lacien
+//
+//  Created by Xavier Vicient on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LacienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
