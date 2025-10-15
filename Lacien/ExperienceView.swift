bd12a19296068e65ccb90d2fb1276924200d9702
Xavier Vicient <xvicient@gmail.com>
2025-10-15 17:07:05 +0200

    Fixed top corner radius when scrolling

--- Lacien/ExperienceView.swift
+++ Lacien/ExperienceView.swift
@@ -3,8 +3,9 @@ import SwiftUI
 struct ExperienceView: View {
     var contentPadding: CGFloat
     var isScrolled: (Bool) -> ()
-    @State private var isPageScrolled: Bool = false
+    var topPadding: CGFloat = 60
     
+    @State private var isPageScrolled: Bool = false
     @State private var scrollProperties: ScrollGeometry = .init(
         contentOffset: .zero,
         contentSize: .zero,
@@ -18,8 +19,7 @@ struct ExperienceView: View {
                 Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac blandit eros, nec luctus nulla. Quisque ante nulla, pulvinar eu sem a, vulputate accumsan dui. Fusce mattis ex vel purus sodales rutrum. Integer fringilla egestas diam luctus aliquam. Mauris ut massa ut ipsum porttitor porta non nec lectus. Phasellus a gravida erat, a varius purus. Maecenas tincidunt nibh a arcu elementum suscipit. Maecenas vel efficitur sapien. Phasellus malesuada, sem id malesuada feugiat, felis lacus interdum lorem, sed tincidunt arcu massa et enim. Vivamus fringilla aliquet nulla malesuada convallis. Cras iaculis pulvinar leo.\n\nDonec auctor sapien a eros porttitor, at rhoncus neque semper. Morbi lobortis et purus eu dictum. Donec consequat interdum fermentum. Pellentesque massa ex, auctor at semper nec, cursus nec nisi. Praesent tincidunt velit id ex ultricies lacinia. Duis vitae suscipit elit. Integer non est blandit, sollicitudin ex nec, mollis tortor. Curabitur tincidunt interdum bibendum. In eu varius libero, vel placerat tellus.\n\nCras id interdum metus. Nullam interdum, ligula ac finibus bibendum, risus metus luctus leo, maximus volutpat libero mi eget elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean sed facilisis neque, et ullamcorper neque. Vivamus accumsan ullamcorper metus vel mollis. Aliquam maximus porta nunc ut dignissim. Donec id magna congue, elementum tellus eget, malesuada nisl. Mauris dui mi, tincidunt at leo sed, tempor tempus ante. Curabitur in efficitur erat.\n\nCras id interdum metus. Nullam interdum, ligula ac finibus bibendum, risus metus luctus leo, maximus volutpat libero mi eget elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean sed facilisis neque, et ullamcorper neque. Vivamus accumsan ullamcorper metus vel mollis. Aliquam maximus porta nunc ut dignissim. Donec id magna congue, elementum tellus eget, malesuada nisl. Mauris dui mi, tincidunt at leo sed, tempor tempus ante. Curabitur in efficitur erat.\n\nCras id interdum metus. Nullam interdum, ligula ac finibus bibendum, risus metus luctus leo, maximus volutpat libero mi eget elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean sed facilisis neque, et ullamcorper neque. Vivamus accumsan ullamcorper metus vel mollis. Aliquam maximus porta nunc ut dignissim. Donec id magna congue, elementum tellus eget, malesuada nisl. Mauris dui mi, tincidunt at leo sed, tempor tempus ante. Curabitur in efficitur erat.")
                     .font(.body)
                     .foregroundColor(.white)
-                    .padding(.vertical)
-                    .padding(.horizontal, contentPadding + (contentPadding * scrollProperties.topInsetProgress))
+                    .padding()
             }
             .background {
                 Image.keyboard
@@ -27,8 +27,19 @@ struct ExperienceView: View {
                     .aspectRatio(contentMode: .fill)
                     .allowsHitTesting(false)
             }
-            .cornerRadius(20)
+        }
+        .mask {
+            UnevenRoundedRectangle(
+                cornerRadii: .init(
+                    topLeading: contentPadding,
+                    bottomLeading: contentPadding,
+                    bottomTrailing: contentPadding,
+                    topTrailing: contentPadding
+                )
+            )
             .padding(.horizontal, -contentPadding * scrollProperties.topInsetProgress)
+            .padding(.top, -topPadding * scrollProperties.offsetYProgress)
+            .padding(.bottom, -contentPadding * scrollProperties.topInsetProgress)
         }
         .scrollClipDisabled()
         .onScrollGeometryChange(for: ScrollGeometry.self, of: {
@@ -42,8 +53,8 @@ struct ExperienceView: View {
         .onChange(of: isPageScrolled) {
             isScrolled(isPageScrolled)
         }
-        .safeAreaPadding(.top, 60)
-        .safeAreaPadding(.bottom, 20)
+        .safeAreaPadding(.top, topPadding)
+        .safeAreaPadding(.bottom, contentPadding - ( contentPadding * scrollProperties.topInsetProgress))
         .ignoresSafeArea()
     }
 }
@@ -55,7 +66,11 @@ extension ScrollGeometry {
     
     var topInsetProgress: CGFloat {
         guard contentInsets.top > 0 else { return 0 }
-        return max(min(offsetY / contentInsets.top, 1), 0)
+        return max(min(offsetYProgress, 1), 0)
+    }
+    
+    var offsetYProgress: CGFloat {
+        offsetY / contentInsets.top
     }
 }
 
